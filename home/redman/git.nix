aad89a99c9823490a255900735ecc1b7c0950b2a
Paul Redman <piredman@users.noreply.github.com>
2025-10-21 09:32:43 -0600

    testing ssh signing

--- home/redman/git.nix
+++ home/redman/git.nix
@@ -1,11 +1,10 @@
 { config, pkgs, userSettings,... }:
 {
-  # home.file.".ssh/allowed_signers".text =
-  #   "* ${builtins.readFile .ssh/id_ed25519.pub}";
 
   programs = {
     git = {
       enable = true;
+      signing.key = "002C241D7D4F044C";
 
       settings = {
         init.defaultBranch = "main";
@@ -17,9 +16,7 @@
         };
 
         commit.gpgsign = true;
-        gpg.format = "ssh";
-        # gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-        user.signingkey = "~/.ssh/id_ed25519.pub";
+        credential.helper = "libsecret";
       };
 
     };
