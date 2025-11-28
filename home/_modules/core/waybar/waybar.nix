b369bdc5eaa2ff8fb4988219935a55f2a89c1d3d
Paul Redman <piredman@users.noreply.github.com>
2025-11-28 14:34:21 -0700

    add inkscape

--- home/_modules/core/waybar/waybar.nix
+++ home/_modules/core/waybar/waybar.nix
@@ -5,101 +5,67 @@
   ...
 }:
 
-let
-  customOS = {
-    format = "";
-  };
-in
 {
   programs.waybar = {
     enable = true;
-    settings = {
-      DP-1 = {
-        output = "DP-1";
-        layer = "top";
-        position = "right";
-        width = 40;
-        spacing = 5;
+    settings.main = {
+      layer = "top";
+      position = "right";
+      width = 40;
+      spacing = 5;
 
-        modules-left = [
-          "custom/os"
-          "hyprland/submap"
-        ];
-        modules-center = [
-          "hyprland/workspaces"
-        ];
-        modules-right = [
-          "tray"
-        ];
+      modules-left = [
+        "custom/os"
+        "hyprland/submap"
+      ];
+      modules-center = [
+        "hyprland/workspaces"
+      ];
+      modules-right = [
+        "tray"
+        "pulseaudio"
+        "network"
+      ];
 
-        "hyprland/workspaces" = {
-          format = "{name}";
-          format-icons = {
-            active = "";
-            default = "";
-          };
+      "hyprland/workspaces" = {
+        format = "{name}";
+        format-icons = {
+          active = "";
+          default = "";
         };
-
-        "custom/os" = customOS;
       };
 
-      DP-2 = {
-        output = "DP-2";
-        layer = "top";
-        position = "left";
-        width = 40;
-        spacing = 5;
-
-        modules-left = [
-          "custom/os"
-          "hyprland/submap"
-        ];
-        modules-center = [
-          "hyprland/workspaces"
-        ];
-        modules-right = [
-          "pulseaudio"
-          "network"
-          "custom/notifications"
-        ];
-
-        "hyprland/workspaces" = {
-          format = "{name}";
-          format-icons = {
-            active = "";
-            default = "";
-          };
-        };
-
-        "custom/os" = customOS;
-        "network" = {
-          format = "󰛵";
-          format-wifi = "";
-          format-ethernet = "󰛳";
-          format-disconnected = "󰅛";
-          tooltip-format-wifi = "{essid} ({signalStrength}%)";
-          tooltip-format-ethernet = "{ifname} ({ipaddr}/{cidr})";
-          tooltip-format-disconnected = "Disconnected";
-          max-length = 30;
-          on-click = "launch-network";
-        };
-
-        "pulseaudio" = {
-          format = "{icon}";
-          format-muted = "";
-          format-icons = {
-            default = [
-              ""
-              ""
-              ""
-            ];
-          };
-          tooltip-format = "Playing at {volume}%";
-          on-click = "$TERMINAL --class=pulseaudio.wiremix -e wiremix";
-          on-click-right = "pamixer -t";
-        };
-
+      "custom/os" = {
+        format = "";
       };
+
+      "network" = {
+        format = "󰛵";
+        format-wifi = "";
+        format-ethernet = "󰛳";
+        format-disconnected = "󰅛";
+        tooltip-format-wifi = "{essid} ({signalStrength}%)";
+        tooltip-format-ethernet = "{ifname} ({ipaddr}/{cidr})";
+        tooltip-format-disconnected = "Disconnected";
+        max-length = 30;
+        on-click = "launch-network";
+      };
+
+      "pulseaudio" = {
+        format = "{icon}";
+        format-muted = "";
+        format-icons = {
+          default = [
+            ""
+            ""
+            ""
+          ];
+        };
+        tooltip-format = "Playing at {volume}%";
+        on-click = "$TERMINAL --class=pulseaudio.wiremix -e wiremix";
+        on-click-right = "pamixer -t";
+      };
+
     };
 
     style = builtins.readFile ./_waybar.css;
