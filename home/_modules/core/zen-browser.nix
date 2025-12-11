0db88d1206ba254ef304ab2ea1054843a452fd31
Paul Redman <piredman@users.noreply.github.com>
2025-12-11 09:17:30 -0700

    Updates to zen-browser

--- home/_modules/core/zen-browser.nix
+++ home/_modules/core/zen-browser.nix
@@ -2,14 +2,13 @@
   config,
   pkgs,
   userSettings,
-  zen-browser,
   ...
 }:
 
 {
   programs.zen-browser = {
+    # Using https://github.com/0xc000022070/zen-browser-flake
     enable = true;
-    package = (pkgs.wrapFirefox zen-browser.packages.${pkgs.stdenv.hostPlatform.system}."beta-unwrapped" {});
 
     profiles.${userSettings.username} = rec {
       settings = {
@@ -21,7 +20,45 @@
     };
 
     policies = {
-      SearchEngines.Default = "Kagi";
+      # Enhanced privacy and security
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+
+      # HTTPS-Only Mode with exceptions for local development
+      HttpsOnlyMode = "force_enabled";
+      HttpAllowlist = [
+        "http://localhost"
+        "http://127.0.0.1"
+        "http://redmandev.net"
+      ];
+
+      # If settings aren't taking effect, you can manually reset by deleing search.json.mozlz4
+      # rm ~/.zen/redman/search.json.mozlz4
+      SearchEngines = {
+        Add = [
+          {
+            Name = "Kagi";
+            URLTemplate = "https://kagi.com/search?q={searchTerms}";
+          }
+        ];
+        Default = "Kagi";
+      };
+
       ExtensionSettings =
         with builtins;
         let
@@ -40,6 +77,26 @@
           (extension "proton-pass" "78272b6fa58f4a1abaac99321d503a20@proton.me")
           (extension "kagi-search-for-firefox" "search@kagi.com")
         ];
+
+      # Locked preferences for consistency
+      Preferences = {
+        "browser.download.useDownloadDir" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.download.alwaysOpenPanel" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.download.manager.showWhenStarting" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.tabs.warnOnClose" = {
+          Value = false;
+          Status = "locked";
+        };
+      };
     };
   };
 
