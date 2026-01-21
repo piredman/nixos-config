ee9f75f7e2648df31155b59539c0310e63f21947
Paul Redman <piredman@users.noreply.github.com>
2026-01-21 07:43:15 -0700

    godot export templates

--- home/_modules/gamedev/godot.nix
+++ home/_modules/gamedev/godot.nix
@@ -7,7 +7,18 @@
 }:
 
 {
+
   home.packages = with pkgs; [
     godot
+    godot_4-export-templates-bin
   ];
+
+  home.file.".local/share/godot/export_templates/${
+    builtins.replaceStrings [ "-" ] [ "." ] pkgs.godot_4-export-templates-bin.version
+  }".source =
+    pkgs.godot_4-export-templates-bin
+    + "/share/godot/export_templates/${
+      builtins.replaceStrings [ "-" ] [ "." ] pkgs.godot_4-export-templates-bin.version
+    }";
+
 }
