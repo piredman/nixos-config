9c8c8098933392d1bf75145d6a8fc36cfb70e148
Paul Redman <piredman@users.noreply.github.com>
2026-02-26 06:54:28 -0700

    font info

---
+++ hosts/_modules/fonts/blueenext/README.md
@@ -0,0 +1,5 @@
+https://github.com/velvetyne/BluuNext/blob/master/README.md
+https://open-foundry.com/fonts/bluu-next
+
+If you use OFL fonts. You can use them in books, posters, artwork, logos, and on websites, even make 3D objects with the outlines—no acknowledgement is required. You can bundle them in apps or software subject to certain conditions
+https://openfontlicense.org/
--- hosts/_modules/fonts/fonts.nix
+++ hosts/_modules/fonts/fonts.nix
@@ -1,27 +1,28 @@
 {
-    config,
-    lib,
-    pkgs,
-    pkgs-stable,
-    systemSettings,
-    userSettings,
-    ...
+  config,
+  lib,
+  pkgs,
+  pkgs-stable,
+  systemSettings,
+  userSettings,
+  ...
 }:
 
 let
-    bluuNext = pkgs.stdenvNoCC.mkDerivation {
-        pname = "bluu-next";
-        version = "local";
-        src = ./blueenext;
-        dontConfigure = true;
-        dontBuild = true;
-        installPhase = ''
-            mkdir -p $out/share/fonts/opentype/bluunext
-            cp -v *.otf $out/share/fonts/opentype/bluunext/
-        '';
-    };
+  # https://github.com/velvetyne/BluuNext/blob/master/README.md
+  bluuNext = pkgs.stdenvNoCC.mkDerivation {
+    pname = "bluu-next";
+    version = "local";
+    src = ./blueenext;
+    dontConfigure = true;
+    dontBuild = true;
+    installPhase = ''
+      mkdir -p $out/share/fonts/opentype/bluunext
+      cp -v *.otf $out/share/fonts/opentype/bluunext/
+    '';
+  };
 in
 {
-    fonts.fontDir.enable = true;
-    fonts.packages = [ bluuNext ];
+  fonts.fontDir.enable = true;
+  fonts.packages = [ bluuNext ];
 }
