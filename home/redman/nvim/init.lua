ebddc365728df7c6fc01cda29ebd887e3ed01456
Paul Redman <piredman@users.noreply.github.com>
2026-07-25 14:44:17 -0600

    Godot updates

--- home/redman/nvim/init.lua
+++ home/redman/nvim/init.lua
@@ -1,10 +1,12 @@
-if vim.fn.filereadable(vim.fn.getcwd() .. "/project.godot") == 1 then
-  local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
-  if not vim.loop.fs_stat(pipepath) then
-    vim.fn.serverstart(pipepath)
-    vim.notify("Connected neovim as Godot external editor.", vim.log.levels.INFO)
-  else
-    vim.notify("Unable to connected neovim as Godot external editor.", vim.log.levels.WARN)
+if not vim.env.GODOT_LANGSERVER_PORT then
+  if vim.fn.filereadable(vim.fn.getcwd() .. "/project.godot") == 1 then
+    local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
+    if not vim.loop.fs_stat(pipepath) then
+      vim.fn.serverstart(pipepath)
+      vim.notify("Connected neovim as Godot external editor.", vim.log.levels.INFO)
+    else
+      vim.notify("Unable to connected neovim as Godot external editor.", vim.log.levels.WARN)
+    end
   end
 end
 
--- home/redman/nvim/lua/redman/lsp.lua
+++ home/redman/nvim/lua/redman/lsp.lua
@@ -8,19 +8,21 @@ vim.lsp.config("*", {
   capabilities = capabilities,
 })
 
-vim.lsp.config("gdscript", {
-  cmd = function(dispatchers, config)
-    return vim.lsp.rpc.connect("127.0.0.1", 6005)(dispatchers)
-  end,
-  filetypes = { "gd", "gdscript" },
-  root_markers = { "project.godot", ".git" },
-  capabilities = capabilities,
-  settings = {},
-  on_attach = function(client, bufnr)
-    vim.notify('gdscript LSP attached!', vim.log.levels.INFO)
-  end,
-})
-vim.lsp.enable("gdscript")
+if not vim.env.GODOT_LANGSERVER_PORT then
+  vim.lsp.config("gdscript", {
+    cmd = function(dispatchers, config)
+      return vim.lsp.rpc.connect("127.0.0.1", 6005)(dispatchers)
+    end,
+    filetypes = { "gd", "gdscript" },
+    root_markers = { "project.godot", ".git" },
+    capabilities = capabilities,
+    settings = {},
+    on_attach = function(client, bufnr)
+      vim.notify("gdscript LSP attached!", vim.log.levels.INFO)
+    end,
+  })
+  vim.lsp.enable("gdscript")
+end
 
 local keymap = vim.keymap
 vim.api.nvim_create_autocmd("LspAttach", {
