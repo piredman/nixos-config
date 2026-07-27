712c638b1b27ab0d7cd2e7d4f6ef9793ee632ddd
Paul Redman <piredman@users.noreply.github.com>
2026-07-27 16:46:02 -0600

    Only lock screen

--- home/_modules/wm/stasis/stasis.nix
+++ home/_modules/wm/stasis/stasis.nix
@@ -12,23 +12,6 @@ let
     exec ${pkgs.systemd}/bin/loginctl lock-session "$session_id"
   '';
 
-  stasisDpms = pkgs.writeShellScriptBin "stasis-dpms" ''
-    case "''${XDG_CURRENT_DESKTOP:-}" in
-      niri)
-        if [ "''${1:-}" = "off" ]; then
-          exec ${pkgs.niri}/bin/niri msg action power-off-monitors
-        fi
-        exec ${pkgs.niri}/bin/niri msg action power-on-monitors
-        ;;
-      Hyprland|hyprland)
-        exec ${pkgs.hyprland}/bin/hyprctl dispatch dpms "''${1:-}"
-        ;;
-      *)
-        echo "Unsupported compositor: ''${XDG_CURRENT_DESKTOP:-unset}" >&2
-        exit 1
-        ;;
-    esac
-  '';
 in
 {
   home.packages = [ lockDisplaySession ];
@@ -38,7 +21,6 @@ in
     extraConfig = builtins.readFile ./stasis.rune;
     extraPathPackages = [
       lockDisplaySession
-      stasisDpms
     ];
   };
 }
